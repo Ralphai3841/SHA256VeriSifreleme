64f81827b16d4ea77271cbd72f4affe3ef6f5c5a
ralpd <ralpd@LAPTOP-FB9086OP>
2023-10-20 15:46:53 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace SHA256VeriSifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.textBoxSifre = new System.Windows.Forms.TextBox();
+            this.btnKaydol = new System.Windows.Forms.Button();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(179, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(232, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre :";
+            // 
+            // textBoxKullaniciAdi
+            // 
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(315, 108);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(286, 27);
+            this.textBoxKullaniciAdi.TabIndex = 2;
+            // 
+            // textBoxSifre
+            // 
+            this.textBoxSifre.Location = new System.Drawing.Point(315, 163);
+            this.textBoxSifre.Name = "textBoxSifre";
+            this.textBoxSifre.Size = new System.Drawing.Size(286, 27);
+            this.textBoxSifre.TabIndex = 3;
+            // 
+            // btnKaydol
+            // 
+            this.btnKaydol.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnKaydol.Location = new System.Drawing.Point(315, 248);
+            this.btnKaydol.Name = "btnKaydol";
+            this.btnKaydol.Size = new System.Drawing.Size(128, 52);
+            this.btnKaydol.TabIndex = 4;
+            this.btnKaydol.Text = "Kayıt Ol";
+            this.btnKaydol.UseVisualStyleBackColor = false;
+            this.btnKaydol.Click += new System.EventHandler(this.btnKaydol_Click);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnGiris.Location = new System.Drawing.Point(472, 248);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(129, 52);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.btnKaydol);
+            this.Controls.Add(this.textBoxSifre);
+            this.Controls.Add(this.textBoxKullaniciAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
+        private System.Windows.Forms.TextBox textBoxSifre;
+        private System.Windows.Forms.Button btnKaydol;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SHA256VeriSifreleme
+{
+
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-FB9086OP;Initial Catalog=SHA256;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxSifre.PasswordChar = '*';
+        }
+
+
+        private string sha256KoduOlustur(string s)
+        {
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+            var sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        private void btnKaydol_Click(object sender, EventArgs e)
+        {
+            if (textBoxKullaniciAdi.Text.ToString().Length == 0
+                    || textBoxSifre.Text.ToString().Length == 0)
+            {
+                MessageBox.Show("Alanlar boş bırakılmaz!");
+                return;
+            }
+
+            string sorgu = "SELECT KullaniciAdi FROM Kullanici WHERE KullaniciAdi = @P1";
+
+            try
+            {
+                baglanti.Open();
+                SqlCommand sqlCommand = new SqlCommand(sorgu, baglanti);
+                sqlCommand.Parameters.AddWithValue("@P1", textBoxKullaniciAdi.Text);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                bool yeniKullaniciEkle = false;
+
+                if (reader.HasRows)
+                {
+                    // Bu isimde bir kullanıcı varsa veritabanına ekleme yapılmaz
+                    MessageBox.Show(textBoxKullaniciAdi.Text + " isminde bir kullanıcı zaten mevcut, ekleme yapılamadı!");
+                }
+                else
+                {
+                    // Veritabına yeni kullnaıcıyı ekleyebiliriz
+                    yeniKullaniciEkle = true;
+                }
+                reader.Close();
+
+                if (yeniKullaniciEkle)
+                {
+                    sqlCommand = new SqlCommand("INSERT INTO Kullanici VALUES (@P1, @P2)", baglanti);
+                    sqlCommand.Parameters.AddWithValue("@P1", textBoxKullaniciAdi.Text);
+                    sqlCommand.Parameters.AddWithValue("@P2", sha256KoduOlustur(textBoxSifre.Text));
+                    sqlCommand.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("VT baglantisinda dorun oluştu, hata kodu: H001\n" + ex.Message);
+            }
+            finally
+            {
+                if (baglanti != null)
+                    baglanti.Close();
+            }
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (textBoxKullaniciAdi.Text.ToString().Length == 0
+                    || textBoxSifre.Text.ToString().Length == 0)
+            {
+                MessageBox.Show("Alanlar boş bırakılmaz!");
+                return;
+            }
+
+            try
+            {
+                baglanti.Open();
+                string sorgu = "SELECT KullaniciAdi, Sifre FROM Kullanici WHERE KullaniciAdi = @P1 " +
+                               "AND Sifre = @P2";
+                SqlCommand sqlCommand = new SqlCommand(sorgu, baglanti);
+                sqlCommand.Parameters.AddWithValue("@P1", textBoxKullaniciAdi.Text);
+                sqlCommand.Parameters.AddWithValue("@P2", sha256KoduOlustur(textBoxSifre.Text));
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Kullanıcı adı ve şifre Doğru! Sisteme Hoş Geldiniz!!");
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı veya şifre hatalı!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("VT baglantisinda dorun oluştu, hata kodu: H002\n" + ex.Message);
+            }
+            finally
+            {
+                if (baglanti != null)
+                    baglanti.Close();
+            }
+        }
+    }
+}
